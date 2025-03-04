63952df81fc9d0d8d78dc5d3d6b8f7cf183f5722
Fahmy Abdul <firstfahmyabdul@gmail.com>
2025-03-04 23:55:59 +0700

    feat: add UI Layout Dashboard Admin & Member

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -9,10 +9,12 @@ interface PropTypes {
 const AuthLayout = (props: PropTypes) => {
     const {children, title} = props;
     return (
-        <Fragment>
+        <div className="flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py">
             <PageHead title={title}/>
-            <section className="max-w-screen-3xl 3xl:container p-6">{children}</section>
-        </Fragment>
+            <section className="max-w-screen-3xl 3xl:container p-6">
+                {children}
+            </section>
+        </div>
     )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,8 +27,7 @@ export default function App({ Component, pageProps: {session, ...pageProps} }: A
       <QueryClientProvider client={queryClient}>
         <NextUIProvider>
         <main className={cn(
-            inter.className,
-            "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py"
+            inter.className
           )}
         >
           <Component {...pageProps} />
